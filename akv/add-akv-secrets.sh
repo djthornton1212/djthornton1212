29ba14c13f89227272953a9e2dd2b4da2adf4326
Dominique J. Thornton <dthornton@caregility.com>
2022-09-15 09:14:27 -0400

    Adding AKV stuff

---
+++ akv/add-akv-secrets.sh
@@ -0,0 +1,12 @@
+#! /usr/local/Cellar/bash/5.1.16/bin/bash
+
+VAULT_NAME='YOUR-VAULT-NAME'
+
+while IFS='|' read -r key value; do
+    echo "Adding Key: $key :: Value: $value"
+    az keyvault secret set \
+    --name "${key//_/-}" \
+    --vault-name "$VAULT_NAME" \
+    --value "$value" \
+    --encoding utf-8
+done < "./akv-secrets-list.txt"
