8b7ac627c10216a6db2b8c78c81e75e06ff5be9d
djthornton1212 <dthornton@caregility.com>
2024-03-05 13:14:50 -0600

    ci: :wrench: Updating get-akv-secret.sh

--- Azure/akv/get-akv-secret.sh
+++ Azure/akv/get-akv-secret.sh
@@ -7,11 +7,25 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 VAULT_NAME='YOUR-VAULT-NAME'
 
 while read -r line; do
+    [[ "$DEBUG" ]] && echo "Retrieving secret for $line"
+    [[ "$DEBUG" ]] && echo "-----------------------------------"
     GET_SECRET=$(az keyvault secret show \
-    --name "$line" \
-    --vault-name "$VAULT_NAME" \
-    --query "value" \
-    --output tsv)
-    echo "Retrieving secret for $line | $GET_SECRET"
+        --name "$line" \
+        --vault-name "$VAULT_NAME" \
+        --query "value" \
+        --output tsv 2>&1
+    )
+
+    echo "-----------------------------------"
+    if [[ "$GET_SECRET" == "ERROR"* ]]; then
+        CATCH_ERROR=$(grep -o 'ERROR:.*vault.' <<< "$GET_SECRET")
+        CATCH_CODE=$(grep 'Code:' <<< "$GET_SECRET")
+        echo "$CATCH_ERROR"
+        echo "$CATCH_CODE"
+        continue
+    fi
+
+    echo "Secret Name: $line"
+    echo "Secret Value: $GET_SECRET"
 
 done < "$SCRIPT_DIR/get-akv-secrets-list.txt"
