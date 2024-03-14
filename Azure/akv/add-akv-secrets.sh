b2881203630fc8ad5c18e517772603f5a0a44a91
djthornton1212 <dthornton@caregility.com>
2024-03-14 09:51:00 -0500

    fix(security): :lock: mask secrets; add-akv-secrets

--- Azure/akv/add-akv-secrets.sh
+++ Azure/akv/add-akv-secrets.sh
@@ -4,10 +4,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 VAULT_NAME='YOUR-VAULT-NAME'
 
 while IFS='|' read -r key value; do
-    echo "Adding Key: $key :: Value: $value"
-    az keyvault secret set \
+    masked_value="${value:0:5}*****"
+    echo "Adding Key: $key :: Value: $masked_value"
+
+    SECRET_SET=$(az keyvault secret set \
     --name "${key//_/-}" \
     --vault-name "$VAULT_NAME" \
     --value "$value" \
-    --encoding utf-8
+    --encoding utf-8)
+    jq '.' <<< "${SECRET_SET/$value/$masked_value}"
 done < "$SCRIPT_DIR/add-akv-secrets-list.txt"
