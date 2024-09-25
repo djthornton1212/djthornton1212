c6dd008e3ce91d92e897357c2f692b3b0b284547
djthornton1212 <dthornton@caregility.com>
2024-09-25 14:55:38 -0500

    feat: :sparkles: Add file uploading
    This commit update the add-akv-secrets.sh to handle uploading
    files along side strings to AKV.

--- Azure/akv/add-akv-secrets.sh
+++ Azure/akv/add-akv-secrets.sh
@@ -4,14 +4,34 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 VAULT_NAME='YOUR-VAULT-NAME'
 
-while IFS='|' read -r key value; do
-    masked_value="${value:0:5}*****"
-    echo "Adding Key: $key :: Value: $masked_value"
+function set_secret() {
+    local key="$1" value="$2" type="${3:-string}"
 
-    SECRET_SET=$(az keyvault secret set \
-    --name "${key//_/-}" \
-    --vault-name "$VAULT_NAME" \
-    --value "$value" \
-    --encoding utf-8)
-    jq '.' <<< "${SECRET_SET/$value/$masked_value}"
+    args=(--name "${key//_/-}"
+        --vault-name "$VAULT_NAME"
+        --encoding utf-8)
+    if [[ "$type" == "string" ]]; then
+        args+=(--value "$value")
+    else
+        args+=(--file "$value")
+    fi
+
+    az keyvault secret set "${args[@]}"
+}
+
+while IFS='|' read -r key value; do
+    type="string"
+    file=''
+    if [[ -f "$SCRIPT_DIR/$value" ]]; then
+        file="$SCRIPT_DIR/$value"
+        value=$(cat "$file");
+        type="file"
+    fi
+    echo "Secret $type detected"
+    masked_value="${value:0:5}*****"
+    echo "Adding Key: $key :: Masked Value: $masked_value"
+
+    SECRET_SET=$(set_secret "$key" "${file:-$value}" $type)
+
+    jq ' .value |= "\(.[0:5])*****"' <<<"$SECRET_SET"
 done < "$SCRIPT_DIR/add-akv-secrets-list.txt"
